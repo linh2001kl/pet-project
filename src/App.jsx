1a57d47e4026c98cbdf7630c3525355dd8284cac
nguyenlinh <nguyenkhanhlinh2001nd@gmail.com>
2025-02-25 17:43:34 +0900

    Done task check again Login/Signin/Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./../../pet-project/src/client/Signup";
-import Login from "./../../pet-project/src/client/Login";
-import Dashboard from "./../../pet-project/src/client/Dashboard";
+import Signup from "./client/Signup";
+import Login from "./client/Login";
+import Dashboard from "./client/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
